dfedb401befff5128ed73f09dfede5a33565ab3c
Brandon <braimond@trinity.edu>
2025-03-23 16:59:23 -0500

    help

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: '',//put your own password
+    password: 'Quietgriffin23',//put your own password
     database: 'socialcircles'
 });
 
